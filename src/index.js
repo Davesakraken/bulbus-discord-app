e0ce09a440ca397e8e4f7d2c9a62bfa91e4846b2
Dave <davidgeorgebell1995@outlook.com>
2023-04-02 14:37:30 +0100

    poll-command

--- src/index.js
+++ src/index.js
@@ -1,39 +1,89 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-import { Client, IntentsBitField } from "discord.js";
+import { Client, IntentsBitField, EmbedBuilder } from "discord.js";
+import { RegisterCommands } from "./register-commands.js"; 
+RegisterCommands();
 
 const client = new Client({
-  intents: [
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMembers,
-    IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.MessageContent,
-  ],
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
 });
 
 client.login(process.env.BOT_TOKEN);
 
-client.on('ready', () => {
+client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
-  });
-
-client.on('messageCreate', (message) => {
-  if(!message.author.bot) {
-    if (message.content.includes('morning')) {
-      message.reply("Morning!")
-    }
-    
-  };
-
-
 });
 
-client.on('interactionCreate', (interaction) => {
-  if(!interaction.isChatInputCommand()) return;
+//Direct Replies
+client.on("messageCreate", (message) => {
+    if (!message.author.bot) {
+        if (message.content.includes("morning")) {
+            message.reply("Morning!");
+        }
+    }
+});
 
-  if (interaction.commandName == "hey") {
-    interaction.reply('hey!')
-  } else if (interaction.commandName == "ping") {
-    interaction.reply('pong!')
-  }
+//Commands
+client.on("interactionCreate", (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === "add") {
+        const num1 = interaction.options.get("first-number").value;
+        const num2 = interaction.options.get("second-number").value;
+        interaction.reply(`The sum of the numbers provided is ${num1 + num2}`);
+    }
+
+    if (interaction.commandName === "poll") {
+        const todaysDate = new Date().toDateString().slice(0, 15);
+
+        const emojis = ["❤️", "💛", "💚", "💙", "💜", "🧡"]
+
+        const title = interaction.options.get("title", true)?.value;
+        const discription = interaction.options.get("description", true)?.value;
+        const option1 = interaction.options.get("first-option", true)?.value;
+        const option2 = interaction.options.get("second-option", true)?.value;
+        const option3 = interaction.options.get("third-option", false)?.value;
+        const option4 = interaction.options.get("fourth-option", false)?.value;
+        const option5 = interaction.options.get("fourth-option", false)?.value;
+        const option6 = interaction.options.get("fourth-option", false)?.value;
+
+        console.log(title)
+
+        const embed = new EmbedBuilder()
+            .setTitle(`${title}: ${todaysDate}`)
+            .setDescription(discription)
+            .setColor("Random")
+            .setFields(
+                { name: option1, value: emojis[0] },
+                { name: option2, value: emojis[1] },
+            );
+
+            const additionalOptions = [
+                {name: option3, i: 2},
+                {name: option4, i: 3},
+                {name: option5, i: 4},
+                {name: option6, i: 5}
+            ];
+
+            additionalOptions.forEach (option => {
+                if (option.name) {
+                    embed.addFields({name: option.name, value: emojis[option.i]})
+                }
+            });
+
+            const fields = embed.data.fields.length;
+
+        interaction.reply({ embeds: [embed], fetchReply: true})
+        .then((m) => {
+          for (let i = 0; i < fields; i++) {
+            m.react(emojis[i])
+          }
+    })
+        
+    }
 });
--- src/register-commands.js
+++ src/register-commands.js
@@ -3,33 +3,89 @@ dotenv.config();
 import { REST, Routes, ApplicationCommandOptionType } from "discord.js";
 
 const commands = [
-  {
-    name: "add",
-    description: "adds two numbers",
-    options: [
-      {
-        name: "first-number",
-        description: "The First number",
-        type: ApplicationCommandOptionType.Number,
-      },
-      {
-        name: "second-number",
-        description: "The Second number",
-        type: ApplicationCommandOptionType.Number,
-      }
-    ],
-  },
+    {
+        name: "add",
+        description: "adds two numbers",
+        options: [
+            {
+                name: "first-number",
+                description: "The First number",
+                type: ApplicationCommandOptionType.Number,
+                required: true,
+            },
+            {
+                name: "second-number",
+                description: "The Second number",
+                type: ApplicationCommandOptionType.Number,
+                required: true,
+            },
+        ],
+    },
+    {
+        name: "poll",
+        description: "create an embed with reactions",
+        options: [
+            {
+                name: "title",
+                description: "title of the poll",
+                type: ApplicationCommandOptionType.String,
+                required: true
+            },
+            {
+                name: "description",
+                description: "description of the poll",
+                type: ApplicationCommandOptionType.String,
+                required: true
+            },
+            {
+                name: "first-option",
+                description: "name of the first option",
+                type: ApplicationCommandOptionType.String,
+                required: true
+            },
+            {
+                name: "second-option",
+                description: "name of the second option",
+                type: ApplicationCommandOptionType.String,
+                required: true
+            },
+            {
+                name: "third-option",
+                description: "name of the third option",
+                type: ApplicationCommandOptionType.String,
+                required: false
+            },
+            {
+                name: "fourth-option",
+                description: "name of the fourth option",
+                type: ApplicationCommandOptionType.String,
+                required: false
+            },
+            {
+                name: "fith-option",
+                description: "name of the fourth option",
+                type: ApplicationCommandOptionType.String,
+                required: false
+            },
+            {
+                name: "sixth-option",
+                description: "name of the fourth option",
+                type: ApplicationCommandOptionType.String,
+                required: false
+            },
+        ],
+    },
 ];
 
 const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
 
-(async () => {
-  try {
-    console.log("Registering slash commands...");
-    await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands });
+export async function RegisterCommands() {
+    try {
+        console.log("Registering slash commands...");
+        await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands });
 
-    console.log("Slash commands registered");
-  } catch (error) {
-    console.log(`ERROR:${error}`);
-  }
-})();
+        console.log("Slash commands registered");
+    } catch (error) {
+        console.log(`ERROR:${error}`);
+    }
+};
